eecada8e7c95e9ce48189a4f7f7ca09e764fc9a1
Fablena <fablena_dias@hotmail.com>
2022-07-30 22:34:22 -0300

    :books: docs: Creating the '.env' folder to store the MySQL data and encoding the atrributes of the other folders to load the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
+import { ProductsController } from 'src/product/productController';
+import { EmployeesController } from 'src/employee/employeeController';
+import { Product } from 'src/product/productModel';
+import { Employee } from 'src/employee/employeeModel';
+import { ProductsService } from 'src/product/productService';
+import { EmployeesService } from 'src/employee/employeeService';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE,
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Product]),
+    SequelizeModule.forFeature([Employee]),
+  ],
+  controllers: [AppController, ProductsController, EmployeesController],
+  providers: [AppService, ProductsService, EmployeesService],
 })
 export class AppModule {}
